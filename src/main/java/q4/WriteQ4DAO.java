af922b010a6350c28f96d4dda4a94c8cb3705c13
Cady Wang <siqiw1@andrew.cmu.edu>
2016-04-05 19:55:17 -0400

    implement Write DB version

--- src/main/java/q4/WriteQ4DAO.java
+++ src/main/java/q4/WriteQ4DAO.java
@@ -1,73 +1,119 @@
 package q4;
 
-import utility.Utility;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import static utility.Utility.GetConnection;
+import static utility.Utility.InitializePooler;
+import static utility.Utility.ReleaseConnection;
+import static utility.Utility.connectionPool;
 
 /**
  * @author Siqi Wang siqiw1 on 4/5/16.
  */
 public class WriteQ4DAO {
-    private static Logger logger = Logger.getLogger("Phase3_Q4");
+    private static final String TABLE_NAME = "newtweets";
+    private static final Set<String> storedTweetIds = new HashSet<>();
 
-    private static final String dns1 = "";
-    private static final String dns2 = "";
-    private static final String dns3 = "";
-    private static final String dns4 = "";
-    private static final String dns5 = "";
-    private final Map<Integer, String> dcMap = new HashMap<Integer, String>() {{
-        dcMap.put(1, dns1);
-        dcMap.put(2, dns2);
-        dcMap.put(3, dns3);
-        dcMap.put(4, dns4);
-        dcMap.put(5, dns5);
-    }};
 
-    private ConcurrentHashMap<String, Worker> workers = new ConcurrentHashMap<>();
-    private ConcurrentLinkedQueue<Worker> idleWorkers = new ConcurrentLinkedQueue<>();
-    private Utility.Cache<String, HashMap<String, String>> caches = new Utility.Cache();
-
-    /**
-     * Worker thread to process requests for same key, as it should be serialized.
-     */
-    private class Worker extends Thread {
-        private String key = null;
-        private final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
-
-        public Worker() {
-            this.start();
-        }
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    tasks.take().run();
-                } catch (Exception e) {
-                    logger.log(Level.WARNING, "Exception is thrown for " + e);
-                }
-                // If requests for one key is done, move the thread from workers to idleWorkers.
-                synchronized (this) {
-                    if (tasks.isEmpty()) {
-                        assert key != null;
-                        workers.remove(key);
-                        idleWorkers.offer(this);
-                        key = null;
-                    }
-                }
-            }
-        }
-
-        public void addTask(Runnable task) {
-            tasks.offer(task);
+    public WriteQ4DAO() {
+        if (connectionPool == null) {
+            InitializePooler();
         }
     }
 
+    String getQuery(String tweetId, String fields, String payload) {
+        String[] fieldList = fields.split(",");
+        String[] payloadList = payload.split(",");
+
+        String query;
+        StringBuilder builder = new StringBuilder();
+        StringBuilder allFields = new StringBuilder();
+        StringBuilder allPayloads = new StringBuilder();
+
+        if (storedTweetIds.contains(tweetId)) {
+            for (int i = 0; i < Math.min(fieldList.length, payloadList.length); i++) {
+                builder.append(fieldList[i] + "=" + payloadList[i] + ",");
+            }
+            query = "UPDATE " + TABLE_NAME + " SET " + toString(builder) + " WHERE tweetid=" + tweetId;
+        } else {
+            for (int i = 0; i < Math.min(fieldList.length, payloadList.length); i++) {
+                allFields.append(fieldList[i] + ",");
+                allPayloads.append(payloadList[i] + ",");
+            }
+            query = "INSERT INTO " + TABLE_NAME + "(" + toString(allFields) + ")" + " VALUES " + "(" + toString
+                    (allPayloads) + ")";
+            storedTweetIds.add(tweetId);
+        }
+        return query;
+    }
+
+    private String toString(StringBuilder allFields) {
+        return allFields.toString().substring(0, allFields.length() - 1);
+    }
+
+    void putData(String tweetId, String query) {
+        Statement stmt = null;
+        Connection conn = null;
+        try {
+            conn = GetConnection();
+            stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // release connection to pool after use
+            ReleaseConnection(conn);
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    String getData(String tweetId, String fields) {
+        Statement stmt = null;
+        Connection conn = null;
+        ResultSet rs;
+        StringBuilder builder = new StringBuilder();
+        String[] fieldList = fields.split(",");
+        for (String field : fieldList) {
+            builder.append(field + ",");
+        }
+        String query = toString(builder);
+
+        StringBuilder result = new StringBuilder();
+        try {
+            conn = GetConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT " + query + "FROM" + TABLE_NAME + "WHERE tweetid=" + tweetId);
+            if (rs.next()) {
+                for (String field : fieldList) {
+                    result.append(rs.getString(field) + ",");
+                }
+            }
+            return toString(result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "";
+        } finally {
+            // release connection to pool after use
+            ReleaseConnection(conn);
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
---
+++ src/main/java/q4/WriteQ4Servlet.java
@@ -0,0 +1,109 @@
+package q4;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Siqi Wang siqiw1 on 4/5/16.
+ */
+public class WriteQ4Servlet extends HttpServlet {
+    private static Logger logger = Logger.getLogger("Phase3_Q4");
+
+    private ConcurrentHashMap<String, Worker> workers = new ConcurrentHashMap<>();
+    private ConcurrentLinkedQueue<Worker> idleWorkers = new ConcurrentLinkedQueue<>();
+
+    /**
+     * Worker thread to process requests for same key, as it should be serialized.
+     */
+    private class Worker extends Thread {
+        private String key = null;
+        private final PriorityBlockingQueue<Runnable> tasks = new PriorityBlockingQueue<>();
+
+        public Worker() {
+            this.start();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    tasks.take().run();
+                } catch (Exception e) {
+                    logger.log(Level.WARNING, "Exception is thrown for " + e);
+                }
+                // If requests for one key is done, move the thread from workers to idleWorkers.
+                synchronized (this) {
+                    if (tasks.isEmpty()) {
+                        assert key != null;
+                        workers.remove(key);
+                        idleWorkers.offer(this);
+                        key = null;
+                    }
+                }
+            }
+        }
+
+        public void addTask(Runnable task) {
+            tasks.offer(task);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String tweetId = req.getParameter("tweetid");
+        // set or get
+        final String operation = req.getParameter("op");
+        String sequence = req.getParameter("seq");
+        //comma separated list of fields
+        final String fields = req.getParameter("fields");
+        //comma separated list of base64* encoded fields
+        final String payload = req.getParameter("payload");
+
+        final WriteQ4DAO dao = new WriteQ4DAO();
+        getWorker(tweetId).addTask(new Runnable() {
+            @Override
+            public void run() {
+                if (operation.equals("get")) {
+                    dao.putData(tweetId, dao.getQuery(tweetId, fields, payload));
+                } else {
+                    dao.getData(tweetId, fields);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    /**
+     * When receiving new request, get the thread for specific key from Workers map, if not exists, create new thread.
+     *
+     * @param key
+     * @return worker thread for the key
+     */
+    private synchronized Worker getWorker(String key) {
+        Worker worker = workers.get(key);
+        if (worker == null) {
+            worker = idleWorkers.poll();
+            assert worker.key == null;
+            if (worker == null) {
+                worker = new Worker(); // Worker's constructor should call start()
+            }
+            worker.key = key;
+            workers.put(key, worker);
+        }
+        return worker;
+    }
+
+
+}
